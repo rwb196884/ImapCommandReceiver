6c2ea4d86233cb3754bf8f6c0c73d5688ea78255
richardb <richardb@truthsoftware.co.uk>
2023-11-18 11:36:56 +0000

    Name.Add reason parameter.

--- ImapCommandReceiver.cs
+++ ImapCommandReceiver.cs
@@ -64,7 +64,7 @@ namespace Rwb.ImapCommandReceiver
             if (string.IsNullOrEmpty(command) || command == "void") { return; }
             ProcessStartInfo psi = new ProcessStartInfo();
             psi.FileName = "/bin/sh";
-            psi.Arguments = $"/root/bin/sceneSet.sh {command}";
+            psi.Arguments = $"/root/bin/scene.sh {command} email";
             psi.RedirectStandardOutput = true;
             psi.UseShellExecute = false;
             psi.CreateNoWindow = true;
