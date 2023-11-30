4189cc8a48bfc21cf25a1f24bb5b2d065765cd7b
root <root@calleva.rwb>
2023-11-30 10:43:39 +0000

    Fix run.sh.

--- run.sh
+++ run.sh
@@ -14,10 +14,10 @@ fi
 
 cd "$wd"
 
-if [ -z "$1"]; then
+if [ -z "$1" ]; then
 	dotnet run --launch-profile "ImapCommandReceiver (linux)" --project "${wd}/Rwb.ImapCommandReceiver.csproj"
 elif [ "$1" = "screen" ]; then
-	if [ $( screen -ls | grep ImapCommandReceiver | wc -l ) -eq 0]; then
+	if [ $( screen -ls | grep ImapCommandReceiver | wc -l ) -eq 0 ]; then
 		# need to start
 		logger -s "Starting ImapCommandReceiver on screen."
 		/usr/bin/screen -dm -S ImapCommandReceiver -Logfile /root/screen.ImapCommandReceiver.log "dotnet run --launch-profile \"ImapCommandReceiver (linux, listen)\" --project \"${wd}/Rwb.ImapCommandReceiver.csproj\""
